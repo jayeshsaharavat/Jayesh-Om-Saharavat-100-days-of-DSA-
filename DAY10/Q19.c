0e219392ca2fc7ff81345ef13f12775059c1c223
JAYESH OM SAHARAVAT <jayeshsaharavat1@gmail.com>
2026-02-10 20:59:31 +0530

    Create Q19.c

---
+++ DAY10/Q19.c
@@ -0,0 +1,31 @@
+//Read a string and check if it is a palindrome using two-pointer comparison.
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100];
+    int left, right, flag = 1;
+
+    // Read string
+    scanf("%s", s);
+
+    left = 0;
+    right = strlen(s) - 1;
+
+    // Two-pointer comparison
+    while (left < right) {
+        if (s[left] != s[right]) {
+            flag = 0;
+            break;
+        }
+        left++;
+        right--;
+    }
+
+    if (flag)
+        printf("YES");
+    else
+        printf("NO");
+
+    return 0;
+}
